b0a3ab4c4ccde4047a859ffc3f5a99db4bd7c630
Noe Misael Nieto Arroyo <tzicatl@gmail.com>
2010-05-20 13:35:53 -0500

    Override the footer portlet to add some credits.

--- src/plonetheme.hanging/plonetheme/hanging/browser/viewlets.py
+++ src/plonetheme.hanging/plonetheme/hanging/browser/viewlets.py
@@ -1,2 +1,6 @@
-#from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 #from plone.app.layout.viewlets.common import ViewletBase
+from plone.app.layout.viewlets.common import FooterViewlet
+
+class hanghingFooterViewlet(FooterViewlet):
+    index = ViewPageTemplateFile('templates/footer.pt')
